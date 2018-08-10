6d9f866b10cd637cf670d884855b54d51541a8fe
效帅楠 <815758971@qq.com>
2018-08-10 17:00:16 +0800

    xsn 修改appdelegate

--- Common/Common/AppDelegate/AppDelegate.m
+++ Common/Common/AppDelegate/AppDelegate.m
@@ -19,6 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    NSLog(@"111");
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
     
